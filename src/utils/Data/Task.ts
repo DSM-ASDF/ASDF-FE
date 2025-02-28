ed404aa3960278eb795a55e892d08eb62b8232fe
limda00 <dylim7715@gmail.com>
2025-02-28 09:25:04 +0900

    🎨 :: 드롭다운 공통 데이터 추가

---
+++ src/utils/Data/Task.ts
@@ -0,0 +1,25 @@
+export const Major = [
+  'FrontEnd',
+  'BackEnd',
+  'Android',
+  'iOS',
+  'Design',
+  'AI',
+  'Game',
+  'Flutter',
+  'Embedded',
+  'DevOps',
+  'Security',
+  'PM',
+  'QA'
+];
+
+export const WorkArea = [
+  '버그',
+  '기능 개발',
+  '퍼블리싱',
+  '기획',
+  '문서화',
+  '테스트',
+  '배포'
+]
