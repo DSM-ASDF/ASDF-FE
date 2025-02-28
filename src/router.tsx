c0a2adc104d8f5d7f66017aa983c09a4b9542a69
limda00 <dylim7715@gmail.com>
2025-02-28 09:22:11 +0900

    💄 :: 레이아웃 구성

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle.style";
 import { CalendarWithTask } from "./pages/CalendarWithTask";
+import { Sprint } from "./pages/Sprint";
+import { Layout } from "./components/Layout";
 
 function router() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/calendar" element={<CalendarWithTask />} />
+        <Route element={<Layout />}>
+          <Route path="/calendar" element={<CalendarWithTask />} />
+          <Route path="/sprint" element={<Sprint />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
