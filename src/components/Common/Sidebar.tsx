bd03a666d381cca635e389047c265808ada372a9
은지 <wej0513@dsm.hs.kr>
2025-03-05 19:58:38 +0900

    Auto stash before rebase of "main" onto "origin/main"

---
+++ src/components/Common/Sidebar.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Sidebar = () => {
+    const 
+  return (
+    <Container>
+      <Top>
+        <Logo />
+        <MyTeams />
+      </Top>
+      
+    </Container>
+  );
+};
+
+
